066f2f6154fb968c822fdf9fd3274847f767b053
Phzin <pedrofeliz2701@gmail.com>
2024-11-07 11:28:38 -0300

    Add files via upload

---
+++ teste html/assets/img/script/main.js
@@ -0,0 +1,10 @@
+const myImage = document.getElementById("Img");
+
+myImage.onclick = () => {
+  const mySrc = myImage.getAttribute("src");
+  if (mySrc === "assets/img/imagem-clique.png") {
+    myImage.setAttribute("src", "assets/img/tbm-te-amo.png");
+  } else {
+    myImage.setAttribute("src", "assets/img/imagem-clique.png");
+  }
+};
